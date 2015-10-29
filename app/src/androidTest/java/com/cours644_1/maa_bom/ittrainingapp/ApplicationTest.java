87e8b6f75ce5744ab927ab4ebcb6036f73f0e102
ArnaudManuel <arnaud.manuel@netplus.ch>
2015-10-29 11:30:27 +0100

    initial commit containing blank app

---
+++ app/src/androidTest/java/com/cours644_1/maa_bom/ittrainingapp/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.cours644_1.maa_bom.ittrainingapp;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
