0e588b667dd09c16c989828f5a55efcf079061f8
ArnaudManuel <arnaud.manuel@netplus.ch>
2015-12-01 19:20:32 +0100

    customactivity

--- app/src/main/java/com/cours644_1/maa_bom/ittrainingapp/CustomActivity.java
+++ app/src/main/java/com/cours644_1/maa_bom/ittrainingapp/CustomActivity.java
@@ -1,12 +1,21 @@
 package com.cours644_1.maa_bom.ittrainingapp;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 
 /**
  * Created by arnaud on 01.12.2015.
  */
 public abstract class CustomActivity extends Activity {
+
+    protected void onResume() {
+        super.onResume();
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    }
+
+
+
 
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
